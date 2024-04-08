0848df3a8dcf06be55ab5e29ce03879a21caf77a
Jerick Dela Rosa <delarosajerick26@gmail.com>
2024-04-09 00:54:46 +0800

    feat: initial navigation & sections
    - added navbar
    - added section files
    - config vue-router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+/* NAVIGATION */
+import MainNav from '@/components/navigation/MainNav.vue'
+
+/* COMPONENTS */
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,24 +12,39 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
+      components: {
+        default: HomeView,
+        navigation: MainNav,
+      },
       meta: {
-        title: 'Main',
+        title: 'Test',
       }
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/section',
+      name: 'section',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/SectionView.vue'),
+      meta: {
+        title: 'Section',
+      }
+    },
+    {
+      path: '/:catchAll(.*)*',
+      name: 'page-not-found',
+      component: () => import('../views/errors/PageNotFound.vue'),
+      meta: {
+        title: 'Page Not Found'
+      }
     }
-  ]
+  ],
+
 })
 
-router.beforeEach((to, from ,next) => {
-  document.title = `${to.meta.title} | Web Developer`
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} | Online Portfolio`
 
   next()
 
