d4e53208afbd3c6706eb9cbfde44fc5490f78d2f
Jerick Dela Rosa <delarosajerick26@gmail.com>
2024-04-08 17:24:13 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'primaryText': "#374151",
+        'secondaryText': "#4b5563"
+      },
+    },
+    screens: {
+      'xs': '290px',
+      // => @media (min-width: 290px) { ... }
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
