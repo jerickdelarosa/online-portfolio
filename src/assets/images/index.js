76ded0dc98dc2c23ba8eb597761b4bb3da3efab8
Jerick Dela Rosa <delarosajerick26@gmail.com>
2024-04-15 11:39:57 +0800

    style: additional spacing fix for hero elements
    - fix multiple viewport spacing
    - adjust some margin and padding spacing

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,7 @@
+import heroImage from './jerick-delarosa-hero.jpg'
 import codingLaptopImg from './coding-laptop-img.jpeg'
 
 export {
+    heroImage,
     codingLaptopImg
 }
--- src/components/sections/HeroSection.vue
+++ src/components/sections/HeroSection.vue
@@ -2,14 +2,13 @@
     <section id="home" class="bg-[#f9f9f9] antialiased xl:h-screen">
         <div class="max-w-screen-xl px-4 pt-24 mx-auto lg:px-6 sm:pt-16 lg:pt-24">
             <div
-                class="container mx-auto flex md:px-24 md:pt-16 md:pb-4 lg:pt-8 lg:px-8 lg:pb-4 xl:px-32 xl:pt-28 xl:pb-8 lg:flex-row flex-col items-center">
-                <!-- <div class="lg:max-w-lg lg:w-full mb-5 md:mb-0 md:w-1/2 w-3/6 md:order-2 flex justify-center"> -->
+                class="container mx-auto flex md:px-24 md:pt-16 md:pb-4 lg:pt-8 lg:pr-8 lg:pb-4 xl:px-32 xl:pt-28 xl:pb-8 lg:flex-row flex-col items-center">
                 <div class="lg:w-full mb-0 flex justify-center lg:order-2">
                     <img class="object-cover object-center rounded-full w-60 h-60 xs:w-72 xs:h-72 lg:w-80 lg:h-80 border-4 border-gray-700"
                         alt="hero_img" :src="heroImage">
                 </div>
                 <div
-                    class="lg:flex-grow mt-12 mb-12 md:mb-6 lg:mb-0 lg:mt-0 lg:w-1.5/2 lg:pr-12 lg:pl-8 xl:pl-12 flex flex-col lg:items-start lg:text-left items-center text-center lg:order-1">
+                    class="lg:flex-grow mt-12 mb-12 md:mb-6 lg:mb-0 lg:mt-0 lg:w-1.5/2 lg:pl-8 xl:pl-20 flex flex-col lg:items-start lg:text-left items-center text-center lg:order-1">
                     <h1
                         class="text-3xl font-extrabold leading-9 tracking-tight mb-3 text-gray-900 sm:text-4xl sm:leading-10 md:text-5xl md:leading-normal">
                         Hi, I'm Jerick
