3081097cceb021ef7df76e98660e0703f701c3a2
Jerick Dela Rosa <delarosajerick26@gmail.com>
2024-04-16 15:28:33 +0800

    style: follow updates

--- src/components/sections/ProjectsSection.vue
+++ src/components/sections/ProjectsSection.vue
@@ -13,14 +13,14 @@
             <div class="py-16">  
                 <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
                     <div :class="`space-y-6 lg:space-y-0 lg:flex lg:items-center lg:gap-12 ${(projectList.length - 1) > index ? 'mb-20' : ''}  text-center lg:text-start`" v-for="(project, index) in projectList" :key="`project-${index}`">
-                        <div :class="`md:6/12 lg:w-6/12 ${(index %= 2) == 0 ? 'order-1 lg:order-2' : ''} flex justify-center hover:opacity-75 cursor-pointer`">
+                        <div :class="`lg:w-6/12 ${(index %= 2) == 0 ? 'order-1 lg:order-2' : ''} flex justify-center`">
                             <img src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="project-img" loading="lazy" class="rounded-lg h-auto md:w-[28rem] lg:w-auto">
                         </div>
                         
-                        <div :class="`md:6/12 lg:w-6/12 ${(index %= 2) == 0 ? 'order-2 lg:order-1' : ''}`">
+                        <div :class="`lg:w-6/12 ${(index %= 2) == 0 ? 'order-2 lg:order-1' : ''}`">
                             <h1 class="font-extrabold uppercase text-2xl mb-5 text-[#2d2e32]">{{ project.title }}</h1>
                             <p class="mt-2 text-gray-600 md:max-w-[32rem] mx-auto lg:mx-0"> {{ project.description }} </p>
-                            <div class="mt-6 flex justify-center lg:justify-start gap-1">
+                            <div class="mt-6 flex justify-center lg:justify-start gap-1 flex-wrap">
                                 <span v-for="stack in project.tech" :key="stack.id" class="inline-flex items-center rounded-md bg-[#0E6CC2] px-2 py-1 text-xs text-white font-extrabold tracking-widest">{{ stack.name }}</span>
                             </div>
                         </div>
@@ -76,7 +76,7 @@ const projectList = [
     {
         id: 'project-3',
         title: 'Online Event Registration',
-        description: 'A website application for Philippine Charity Sweepstakes Office to track, process and dispose organizational document transactions.',
+        description: 'A digital platform designed to facilitate the seamless registration process for citizens attending agency-hosted events and activities. Through this system, individuals can easily sign up for events, workshops, and seminars, providing necessary information and preferences.',
         tech: [
             {
                 id: 'stack-1',
@@ -95,7 +95,7 @@ const projectList = [
     {
         id: 'project-4',
         title: 'Document Tracking System',
-        description: 'A website application for Bulacan State University that tracks their organizational document transactions.',
+        description: 'A tracking system designed to efficiently manage and monitor the flow of important documents within the institution. It streamlines processes such as submission, review, approval, ensuring transparency, accountability, and compliance with relevant regulations.',
         tech: [
             {
                 id: 'stack-1',
